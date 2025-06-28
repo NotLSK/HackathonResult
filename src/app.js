1ac8892b285f3e4efefbc8c479faaeb409507e8a
Egor Razzorenov <razzorenov08@gmail.com>
2025-06-28 18:17:13 +0300

    Main page

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
 import './styles.css'
 import { ShapeModule } from './modules/shape.module'
+import { Page } from './pages/main/main';
+import { BackgroundModule } from './modules/background.module';
+
+
 
